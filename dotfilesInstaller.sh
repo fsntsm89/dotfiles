bf51d7d3f77c1eb5b538364d197c4ed0c9e93a60
Keisuke Ito <ito@komatsuelec.co.jp>
2014-11-07 19:18:57 +0900

    add pomodoro

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -7,3 +7,5 @@ ln -sf ~/.dotfiles/.oh-my-zsh/custom/themes/original.zsh-theme ~/.oh-my-zsh/cust
 # emacs設定
 ln -sf ~/.dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
 ln -sf ~/.dotfiles/.emacs.d/Cask ~/.emacs.d/Cask
+# pomodoro
+ln -sf ~/.dotfiles/.emacs.d/elisp/pomodoro.el ~/.emacs.d/elisp/pomodoro.el
