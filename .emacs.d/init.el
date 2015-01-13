d3619b9a7705e17c882c83d744d07b9c30fe351d
Keisuke Ito <ito@komatsuelec.co.jp>
2015-01-14 08:26:42 +0900

    fix smooth-scroll

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -170,8 +170,9 @@
 
 ;; ------------------------------------------------------------------------
 ;; Smooth Scroll
+(setq scroll-step 1)
 (setq scroll-conservatively 10000)
-
+(setq auto-window-vscroll nil)
 
 ;; ------------------------------------------------------------------------
 ;; @ initial frame maximize
